74bc7bb44785287ee972057e6095db3e9cc73179
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-06-13 17:07:01 -0400

    errores en app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AButton @clickHijo="padreHandleClick" />
+  <div>
+    <NewComponent :titulo="'Test de compomnentes con CLI'"
+    :subtitulo="'Esto podría ser un subtítulo'"
+    :datos= '[2, 4, 6, 8, 10]'
+    titulo_tabla="Tabla de números pares"
+    />
+    <NewComponent
+    :titulo="'Y esta es la otra tabla:'"
+    :subtitulo="'pero sin subtítulo'"
+    :datos= '[1, 3, 5, 7, 9]'
+    titulo_tabla="Tabla de números impares" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewComponent from './components/NewComponent.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    NewComponent,
+
+  },
+  methods: {
+    padreHandleClick() {
+      console.log("Click en el Padre")
+
+    }
+
+
   }
 }
 </script>
